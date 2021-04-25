15492b0492fac56ae34d12c833c341aa1538c0e2
LebsterFace <samsalinsky@gmail.com>
2021-04-25 08:20:43 +0100

    format change

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -1,7 +1,7 @@
 module.exports = {
     run: async (client, message, args) => {
         return message.channel.send(
-            "Use /TNTconfigure to setup the bot: \n" +
+            "Use `/TNTconfigure` to set up the bot:\n" +
             "https://discord.com/oauth2/authorize?client_id=735055542178938960&scope=bot&permissions=2147994688"
         );
     },
