0c580149dc0d3d638f2953e6a21f3d7317a29768
LebsterFace <samsalinsky@gmail.com>
2021-05-02 01:46:40 +0100

    Announce to all channels

--- src/commands/announcement.js
+++ src/commands/announcement.js
@@ -1,16 +1,19 @@
 const {errorEmbed, successEmbed, getAvatar} = require("../util.js");
 const config = require("../../config.json");
+const db = require("../db");
 
 module.exports = {
-	run: ({client, message, multiArgs}) => {
+	run: async ({client, message, multiArgs}) => {
 		if (message.author.id !== config.owner_id) {
 			return message.channel.send(errorEmbed("Invalid permissions", "This command can only be executed by the bot owner"));
 		}
-		
-		// TODO: Every configured channel
+
 		const embed = successEmbed(message.author, multiArgs, "__Announcement__", getAvatar(client.user));
-		return message.channel.send(embed);
+		const rows = await db.all(db.TABLES.ConfiguredChannels);
+		return Promise.all(rows.map(({channel}) => 
+			client.channels.fetch(channel).then(c => c.send(embed))
+		));
 	},
-	aliases: [],
+	aliases: ["announce"],
 	requiresConfiguredChannel: false
 };
